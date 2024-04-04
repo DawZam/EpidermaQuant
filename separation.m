c46d038e23e3f98a3b07cee6c5e23e5a0ea62c35
Dawid <148239676+DawZam@users.noreply.github.com>
2024-04-04 11:53:32 +0200

    Delete separation.m

--- separation.m
+++
@@ -1,9 +0,0 @@
-function imageOut = separation(imageRGB, MatrixHDAB)
-    % dekonvolution 
-    imageOut = reshape(-log(imageRGB),[],3) * MatrixHDAB;
-    imageOut = reshape(imageOut, size(imageRGB));
-
-    % normalization
-    imageOut = normalization(imageOut);
-    
-end
